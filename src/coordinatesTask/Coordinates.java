0580eb10d4a18e77c15601fcc8a4dd93d7101627
Kyryll1304 <kirilltrosnickii@me.com>
2023-01-20 23:40:20 +0200

    Initial commit

---
+++ src/coordinatesTask/Coordinates.java
@@ -0,0 +1,9 @@
+package coordinatesTask;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude=50.42657955935711;
+        double longitude=30.56301991166942;
+        System.out.println("Монумент 'Батьківщина-мати' знаходиться за кординатами "+latitude+" широти та "+longitude+" довготи");
+    }
+}
